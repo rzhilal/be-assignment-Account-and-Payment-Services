8fec36ca401c77e0ba32fecb758a97a864e4a5f6
Rizq Hilal Rifaasya <rzqhilal@gmail.com>
2024-08-19 07:39:54 +0700

    Project structure and template

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
+
+const server = Fastify();
+
+server.register(swagger, {
+  swagger: {
+    info: {
+      title: 'Fastify Payment Service API',
+      description: 'API documentation for Payment Management Services',
+      version: '1.0.0',
+    },
+  },
+});
+
+server.register(swaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+});
+
+server.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
